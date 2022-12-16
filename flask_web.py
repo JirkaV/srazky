51a469908fbe149e9d21754aa6e4614bf5a0bb19
Jirka Vejrazka <Jirka.Vejrazka@gmail.com>
2022-12-16 22:39:29 +0100

    workaround for weird bug mangling downloaded files

--- flask_web.py
+++ flask_web.py
@@ -10,6 +10,7 @@ def get_excel(ym):
     if not (2019 <= y < 2100  and 1 <= m <= 12):
         abort(404)
     response = make_response(vyrob_excel(str(ym)))
-    response.headers['Content-Type'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    #response.headers['Content-Type'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    response.headers['Content-Type'] = 'application/octet-stream'  # turns out that the "openxmlformat" triggers a bug in xml2enc when mod_proxy_html is enabled on the front-end webserver, see https://bz.apache.org/bugzilla/show_bug.cgi?id=64339
     response.headers['Content-Disposition'] = 'attachment; filename=Srazky-{}.xlsx'.format(ym)
     return response
